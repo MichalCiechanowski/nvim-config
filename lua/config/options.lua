176fb523e6e819cce59fb9d1280e4b106818c246
Michal Ciechanowski <ciechanowski62@gmail.com>
2024-09-08 16:56:51 +0200

    batman

---
+++ lua/config/options.lua
@@ -0,0 +1,38 @@
+vim.g.mapleader = " "
+vim.g.mallocalleader = " "
+
+vim.g.netrw_banner = 0
+
+vim.opt.guicursor = ""
+vim.opt.cursorline = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.showmode = false
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.undofile = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.hlsearch = true
+
+vim.g.have_nerd_font = true
+vim.opt.guifont = "FiraCode Nerd"
---
+++ lua/config/vimapi.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+vim.api.nvim_create_user_command("W", "w", {})
+vim.api.nvim_create_user_command("Wq", "wq", {})
