7a5ff0a444b93e2df5478fd0bd605c062d3edad2
Michal Ciechanowski <ciechanowski62@gmail.com>
2024-12-11 13:22:08 +0100

    oil hidden changes

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -9,6 +9,12 @@ return {
 		skip_confirm_for_simple_edits = true,
 		view_options = {
 			show_hidden = true,
+			is_hidden_file = function(name, _)
+				return name == "../"
+			end,
+			is_always_hidden = function(name, _)
+				return name == "../"
+			end,
 		},
 	},
 	-- Optional dependencies
