7ab5daaaed4c8812d4d995b92b129b2f12e70aac
Michal Ciechanowski <ciechanowski62@gmail.com>
2024-12-11 10:40:52 +0100

    oil .hidden_files change

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,6 +7,9 @@ return {
 		default_file_explorer = true,
 		delete_to_trash = true,
 		skip_confirm_for_simple_edits = true,
+		view_options = {
+			show_hidden = true,
+		},
 	},
 	-- Optional dependencies
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
