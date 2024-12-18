ae15d273b228406bc140b77e8df243ae81fc4734
Michal Ciechanowski <ciechanowski62@gmail.com>
2024-12-18 16:00:14 +0100

    oil semi works

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,16 +6,15 @@ return {
 		default_file_explorer = true,
 		delete_to_trash = true,
 		skip_confirm_for_simple_edits = true,
-		-- view_options = {
-		-- 	show_hidden = true,
-		-- 	is_hidden_file = function(name)
-		-- 		return name == "../"
-		-- 	end,
-		-- },
 	},
 	vim.keymap.set("n", "<leader>a", function()
+		require("oil").setup({ view_options = { show_hidden = false } })
 		vim.cmd("Oil")
 	end, { desc = "Open parent directory" }),
+	vim.keymap.set("n", "<leader>A", function()
+		require("oil").setup({ view_options = { show_hidden = true } })
+		vim.cmd("Oil")
+	end, { desc = "Open parent directory with .dotfiles" }),
 	-- Optional dependencies
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
