b5b173fb251afe140f02874930459e349a70b180
Michal Ciechanowski <ciechanowski62@gmail.com>
2024-12-18 16:10:47 +0100

    oil fix 1.2

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -8,11 +8,21 @@ return {
 		skip_confirm_for_simple_edits = true,
 	},
 	vim.keymap.set("n", "<leader>a", function()
-		require("oil").setup({ view_options = { show_hidden = false } })
+		require("oil").setup({
+			default_file_explorer = true,
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = true,
+			view_options = { show_hidden = false },
+		})
 		vim.cmd("Oil")
 	end, { desc = "Open parent directory" }),
 	vim.keymap.set("n", "<leader>A", function()
-		require("oil").setup({ view_options = { show_hidden = true } })
+		require("oil").setup({
+			default_file_explorer = true,
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = true,
+			view_options = { show_hidden = false },
+		})
 		vim.cmd("Oil")
 	end, { desc = "Open parent directory with .dotfiles" }),
 	-- Optional dependencies
